0e9cf5ead590f1b205638b47b957f3669e0bcc81
aminul042 <aminul042@gmail.com>
2022-10-11 03:50:47 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+require('../../../bootstrap');
+
+window.Vue = require('vue');
+
+import { store } from './store';
+import helpers from './helpers';
+import filters from '.filters';
+
+// Using global filters for all vue components:
+
+Vue.mixin({
+  filters: filters
+});
+
+// Using custom functions to be available in all components:
+
+const plugin = {
+    install () {
+      Vue.helpers = helpers
+      Vue.prototype.$helpers = helpers
+    }
+}
+
+Vue.use(plugin)
+
+Vue.component('seafarer-status', require('./components/profile.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
